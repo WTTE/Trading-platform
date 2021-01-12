bbdda8a362d1e759ed744403eb6743187e7f42c2
wangsonghan123 <1245652684@qq.com>
2021-01-12 21:50:29 +0800

    代码修改

--- src/views/admin/goods.vue
+++ src/views/admin/goods.vue
@@ -1,3 +1,22 @@
 <template>
     <div>这是商品信息</div>
 </template>
+<script>
+    import {getProductList} from '@/api/product'
+    export default {
+        data(){
+            return {
+                list:[]
+            }
+        },
+        created(){
+            this.getProList();
+        },
+        methods:{
+            async getProList(){
+                let result = await getProductList();
+                console.log(result);
+            }
+        }
+    }
+</script>
